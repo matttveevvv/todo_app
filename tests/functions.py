cb756872608634974627c3cdfe7292440f2f379d
Andrey <matttveevvv@gmail.com>
2025-01-26 15:53:28 +0500

    Create basic GUI without interactions, day 16

--- tests/functions.py
+++ tests/functions.py
@@ -1,10 +1,16 @@
-def get_todos(filepeth = "todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepeth = FILEPATH):
 
     with open("todos.txt", "r") as file:
         todos = file.readlines()
     return todos
 
 
-def write_todos(todos, filepeth = "todos.txt"):
+def write_todos(todos, filepeth = FILEPATH):
     with open("todos.txt", "w") as file:
         file.writelines(todos)
+
+if __name__ == "__maine__":
+    print("Hello")
+    print(get_todos())
